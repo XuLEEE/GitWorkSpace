367288472559df34cb252cc895bc23ad34696214
XuLEEE <103911200+XuLEEE@users.noreply.github.com>
2023-05-23 10:06:48 +0800

    HelloWord添加xuzunle

--- Demo01/src/com/xzl/HelloWord.java
+++ Demo01/src/com/xzl/HelloWord.java
@@ -3,5 +3,6 @@ package com.xzl;
 public class HelloWord {
     public static void main(String[] args) {
         System.out.println("hello word!!!");
+        System.out.println("xuzunle");
     }
 }
