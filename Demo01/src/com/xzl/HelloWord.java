c002dba077606567a723022dcf033e171cf8b441
XuLEEE <103911200+XuLEEE@users.noreply.github.com>
2023-05-23 10:11:38 +0800

    完成修改

--- Demo01/src/com/xzl/HelloWord.java
+++ Demo01/src/com/xzl/HelloWord.java
@@ -4,6 +4,6 @@ public class HelloWord {
     public static void main(String[] args) {
         System.out.println("hello word!!!");
         System.out.println("xuzunle");
-        System.out.println("github")
+        System.out.println("github");
     }
 }
